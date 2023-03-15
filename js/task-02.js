8c90d8c5f20f835407c92ebd1716d0fb0c1b8792
Nikolay Maloshenko <115625550+Nikolay96UA@users.noreply.github.com>
2023-03-15 18:57:31 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+for (let ingredient of ingredients) {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add("item");
+  ingredientsEl.append(liEl);
+}
+
+document.body.append(ingredientsEl);
