9ef257fe2ffaa0523d63b0809337a1410433f4d9
Nikolay Maloshenko <115625550+Nikolay96UA@users.noreply.github.com>
2023-03-16 14:38:22 +0200

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,9 @@
+const inputName = document.querySelector("#name-input");
+const outputName = document.querySelector("#name-output");
+
+inputName.addEventListener("input", (event) => {
+  outputName.textContent = event.currentTarget.value;
+  if (outputName.textContent === "") {
+    return (outputName.textContent = "Anonymous");
+  }
+});
