1c4531fdeb17b2c39cbd9a54cc3a8b7e21ade798
Nikolay Maloshenko <115625550+Nikolay96UA@users.noreply.github.com>
2023-03-17 23:17:54 +0200

    Final

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const inputValue = document.querySelector("input");
+const boxes = document.querySelector("#boxes");
+
+const createBoxes = (amount) => {
+  amount = Number(inputValue.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    const divEl = document.createElement("div");
+    divEl.style.width = `${30 + 10 * i}px`;
+    divEl.style.height = `${30 + 10 * i}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    divEl.style.margin = "5px";
+    boxes.append(divEl);
+  }
+};
+
+const destroyBoxes = () => {
+  while (boxes.firstElementChild) {
+    boxes.firstElementChild.remove();
+  }
+};
+
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
