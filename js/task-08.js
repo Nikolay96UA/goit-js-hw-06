6265455a73b24c0eba1f784d8d4ab1f6b3a2e034
Nikolay Maloshenko <115625550+Nikolay96UA@users.noreply.github.com>
2023-03-17 19:36:06 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formLog = document.querySelector(".login-form");
 
+const submitLoginForm = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  const LogObj = {
+    email: email.value,
+    password: password.value,
+  };
+
+  if (email.value === "" || password.value === "") {
+    return alert("Будь-ласка, заповніть всі поля!");
+  }
+
+  console.log(LogObj);
+
+  event.currentTarget.reset();
+};
+
+formLog.addEventListener("submit", submitLoginForm);
