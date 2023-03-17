a85fc90a6ee45d6a3796218db456f6cc09577795
Nikolay Maloshenko <115625550+Nikolay96UA@users.noreply.github.com>
2023-03-17 20:24:26 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+const outputColor = document.querySelector(".color");
+
+const getBogyColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  outputColor.textContent = getRandomHexColor();
+};
+
+btnChangeColor.addEventListener("click", getBogyColor);
